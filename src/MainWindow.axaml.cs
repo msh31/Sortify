0c61424a5fd2f337a50788169b15134d3989d5cf
Marco <mshendriks03@gmail.com>
2025-01-12 20:12:46 +0100

    Added Functionality

--- src/MainWindow.axaml.cs
+++ src/MainWindow.axaml.cs
@@ -1,11 +1,102 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace src;
 
 public partial class MainWindow : Window
 {
+    private Queue<string> images = new Queue<string>();
+    private string currentImage;
+    private string[] imageTypes = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".heic" };
+    private TextBlock folderText;
+    private TextBlock countText;
+
     public MainWindow()
     {
         InitializeComponent();
+        folderText = this.FindControl<TextBlock>("directoryText");
+        countText = this.FindControl<TextBlock>("imageCountText");
+    }
+
+    private async void OnBrowseClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog();
+        string folder = await dialog.ShowAsync(this);
+
+        if (folder != null)
+        {
+            string[] allFiles = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories);
+            images.Clear();
+
+            foreach (string file in allFiles)
+            {
+                string extension = Path.GetExtension(file).ToLower();
+                foreach (string type in imageTypes)
+                {
+                    if (extension == type)
+                    {
+                        images.Enqueue(file);
+                        break;
+                    }
+                }
+            }
+
+            folderText.Text = Path.GetFileName(folder);
+            countText.Text = "Total: " + images.Count;
+            LoadNextPicture();
+        }
+    }
+
+    private void LoadNextPicture()
+    {
+        if (images.Count > 0)
+        {
+            currentImage = images.Dequeue();
+
+            try
+            {
+                imageDisplay.Source = new Bitmap(currentImage);
+                keepButton.IsEnabled = true;
+                deleteButton.IsEnabled = true;
+                countText.Text = "Images remaining: " + images.Count;
+            }
+            catch
+            {
+                LoadNextPicture();
+            }
+        }
+        else
+        {
+            imageDisplay.Source = null;
+            keepButton.IsEnabled = false;
+            deleteButton.IsEnabled = false;
+            countText.Text = "Done!";
+        }
+    }
+
+    private void OnKeepClick(object sender, RoutedEventArgs e)
+    {
+        LoadNextPicture();
+    }
+
+    private void OnDeleteClick(object sender, RoutedEventArgs e)
+    {
+        if (currentImage != null)
+        {
+            try
+            {
+                imageDisplay.Source = null;
+                File.Delete(currentImage);
+            }
+            catch
+            {
+            }
+
+            LoadNextPicture();
+        }
     }
 }
