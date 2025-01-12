bfcc022ce6ebb67e0aa2be2dc9f8039525c8a255
Marco <mshendriks03@gmail.com>
2025-01-12 20:53:11 +0100

    renamed namespace to sortifyApp

--- src/MainWindow.axaml.cs
+++ src/MainWindow.axaml.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
-namespace src;
+namespace sortifyApp;
 
 public partial class MainWindow : Window
 {
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia;
 using System;
 
-namespace src;
+namespace sortifyApp;
 
 class Program
 {
